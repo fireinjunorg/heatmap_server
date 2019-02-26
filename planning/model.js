2e0215fe6c58e473132b69513deceb0f5b0b8d14
FireInjun <mk@fireinjun.com>
2019-02-26 14:53:52 -0500

    added goal model

--- planning/model.js
+++ planning/model.js
@@ -1,1 +1,24 @@
-import moment from 'moment';
+// Color map
+const COLOR_MAP = {
+  '#196127': 4,
+  '#239a3b': 3,
+  '#7bc96f': 2,
+  '#c6e48b': 1,
+  '#ebedf0': 0,
+};
+
+// Example Reference Model
+const Example = {
+  date: '2018-04-30',
+  count: 2,
+  color: '#c6e48b',
+};
+
+// Goal Model
+const Goal = {
+  date: '2018-04-30',
+  year: 2018,
+  day: 120,
+  count: 2,
+  color: 1,
+};
